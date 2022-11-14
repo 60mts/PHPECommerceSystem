66b411884a59742eae51c0790d6372f15e68b5ae
unknown <60mts@60mtsHome.60mts>
2022-11-14 14:44:23 +0300

    Create menuDetailPge.php

---
+++ eticaret/shoppingTheme/menuDetailPge.php
@@ -0,0 +1,28 @@
+<?php include 'header.php'; ?>
+	<?php include '../funcFile/connect.php';?>
+	<?php
+    $menusor=$db->prepare("SELECT * FROM menu");
+    $menucek=$menusor->execute();
+    
+    
+    ?>
+	
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+                    MENU PAGE
+                    </div>
+            </div>
+</div>
+
+            <div class="container " style="height:550px;">
+
+            <div class="menuDetail">
+
+    <h3> <?php echo $menucek['menuName'];?></h3>
+
+            </div>
+    </div>
+	
+	<?php include 'footer.php';?>
