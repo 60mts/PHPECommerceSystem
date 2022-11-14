9fe04b1390986296a1c14a7f7192de7dfefa9e5f
unknown <60mts@60mtsHome.60mts>
2022-11-14 15:06:05 +0300

    Update menuDetailPge.php

--- eticaret/shoppingTheme/menuDetailPge.php
+++ eticaret/shoppingTheme/menuDetailPge.php
@@ -1,6 +1,5 @@
 <?php include 'header.php'; ?>
-	<?php include '../funcFile/connect.php';?>
-	<?php
+	<?php include '../funcFile/connect.php';
     $menusor=$db->prepare("SELECT * FROM menu");
     $menucek=$menusor->execute();
     
